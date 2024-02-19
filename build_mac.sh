3980a36b17eb59e8dbdfbf805cf6252630fe410e
Eric Helgeson <erichelgeson@gmail.com>
2024-02-19 09:53:05 -0600

    Only build 1 binary as we can build universal now

--- build_mac.sh
+++ build_mac.sh
@@ -11,7 +11,7 @@ fi
 which gcc
 
 declare -a types=("II" "Plus" "SEFDHD")
-declare -a platforms=("mc64" "mcar")
+declare -a platforms=("mc64")
 MAINTAINER="erichelgeson@gmail.com"
 HOMEPAGE="https://bluescsi.com"
 COMMON_FLAGS=${*:-'-n minivmac-3.7-bluescsi -bg 1 '}
@@ -32,6 +32,6 @@ do
                         > setup.sh
                 . ./setup.sh
                 xcodebuild
-                mv minivmac.app dist/minivmac-$platform-$type.app
+                mv minivmac.app dist/minivmac-macOS-$type.app
         done
 done
