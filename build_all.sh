1472d8395925bc2ded09336d40036c2402e62c08
Eric Helgeson <erichelgeson@gmail.com>
2024-02-19 09:33:51 -0600

    minivmac-3.7-2024.02.19

--- build_all.sh
+++ build_all.sh
@@ -5,7 +5,7 @@ set -x
 rm -rf ./dist
 mkdir -p ./dist
 
-COMMON_FLAGS=${1:-'-n minivmac-3.7-2023.09.24 -bg 1 -sony-tag 1 -sony-sum 1 -sony-dc42 1 -sound 1 -drives 20 '}
+COMMON_FLAGS=${1:-'-n minivmac-3.7-2024.02.19 -bg 1 -sony-tag 1 -sony-sum 1 -sony-dc42 1 -sound 1 -drives 20 '}
 
 ./build_mac.sh $COMMON_FLAGS
 ./build_lin.sh $COMMON_FLAGS
